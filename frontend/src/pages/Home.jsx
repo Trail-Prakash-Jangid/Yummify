a372da5108a200ae1b09f95fc3e62d8b91043763
trial-prakash-jangid <kanekiken8333@gmail.com>
2025-05-21 23:06:52 +0530

    some small fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import chef from "../images/chef logo design.jpg";
+import chef from "../images/chef.jpg";
 import Slider from "react-slick";
 import axios from "axios";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { NextArrow, PrevArrow } from "../components/CustomArrows";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { BACKEND_URL } from '../utils/utils.js';
 
 
